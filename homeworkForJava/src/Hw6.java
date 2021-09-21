ac3c69939d91f654f06f1163812152bd0d0bfa73
anastasia-kaz <anastasia-kaz@mail.ru>
2021-09-21 17:19:20 +0300

    hw 6 done

--- homeworkForJava/src/Hw6.java
+++ homeworkForJava/src/Hw6.java
@@ -8,13 +8,13 @@ public class Hw6 {
 //        необходимо вывести все буквы “о” из этой строки.
 //        Для указанной строки ответ будет “ооооо” (или в столбик)
 
-//        String s = "Перестановочный алгоритм быстрого действия";
-//
-//        for (int i = 0; i < s.length(); i++){
-//            if ( s.charAt(i) == 'о' ) {
-//                System.out.print(s.charAt(i));
-//            }
-//        }
+        String s = "Перестановочный алгоритм быстрого действия";
+
+        for (int i = 0; i < s.length(); i++){
+            if ( s.charAt(i) == 'о' ) {
+                System.out.print(s.charAt(i));
+            }
+        }
 
 //        Задача №2
 //
@@ -23,14 +23,14 @@ public class Hw6 {
 //        необходимо подсчитать количество букв “е” в строке.
 //        Для указанной строки ответ будет 4.
 
-//        String s = "Перевыборы выбранного президента";
-//        int count = 0;
-//        for (int i = 0; i < s.length(); i++ ) {
-//            if(s.charAt(i) == 'е') {
-//                count++;
-//            }
-//        }
-//        System.out.println(count);
+        String s1 = "Перевыборы выбранного президента";
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++ ) {
+            if(s1.charAt(i) == 'е') {
+                count++;
+            }
+        }
+        System.out.println(count);
 
 //        Задача №3
 //
@@ -39,45 +39,56 @@ public class Hw6 {
 //        необходимо вывести индексы начала всех подстрок - “рит”, независимо от регистра.
 //        Для указанной строки ответ будет 6, 15, 29.
 
-        String s = "Посмотрите как Рите нравится ритм";
-//
-        s = s.toLowerCase();
-        char arr[] = s.toCharArray();
-        String a = "рит";
-        char arr1[] = a.toCharArray();
-        for (int i = 0; i<arr.length; i++) {
-            if (arr[i] == 'р') {
-                if (arr[i+1] == 'и') {
-                    if (arr[i] + 2 == 'т') {
-                        System.out.println(s.indexOf('р'));
-                    };
-                };
+        String s2 = "Посмотрите как Рите нравится ритм";
+        s2 = s2.toLowerCase();
+        String sub = "рит";
+        for (int i = 0; i < s2.length() - sub.length(); i++) {
+            if (s2.substring(i, i+sub.length()).equals(sub)) {
+                System.out.println(i);
             };
         };
 
+        int index = s2.indexOf("рит");
+        while (index != -1){
+            System.out.println(index);
+            index = s2.indexOf("рит", index + 1);
+        }
+
+//        Экстра задача
+//        Дан массив:
+//        String[][] array = {{“Привет”, “всем”, “кто”}, {“изучает”, “язык”, “программирования”}, {“java”}};
+//        необходимо подсчитать количество строк в массиве, которые не содержат буквы “е”.
+//
+        String[][] array = {{"Привет", "всем", "кто"}, {"изучает", "язык", "программирования"}, {"java"}};
+        int count3 = 0;
+        for (int i = 0; i < array.length; i ++){
+            for (int j = 0; j < array[i].length; j++) {
+                boolean notex = false;
+                for (int k = 0; k < array[i][j].length(); k++) {
+                    if (array[i][j].charAt(k) == 'е') {
+                        notex = true;
+                        break;
+                    }
+                }
+                if (!notex){
+                    count3++;
+                }
+                notex = false;
+
+            }
+        }
+        System.out.println(count3);
 
 
-//        System.out.println(s.indexOf("рит"));
-
-
-
-//        for (int i = 0; i < s.length(); i++){
-//            if (arr[i] == 'р') {
-//                if (arr[i+1] == 'и'){
-//                    if (arr[i]+2 == 'т') {
-//                        System.out.println(s.indexOf(i));
-//                    }
-//                }
-//            }
-//        }
-        //System.out.println(s.indexOf("рит"));
-
-
-
-
-
-
-
+        int count1 = 0;
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                if (!array[i][j].contains("е")){
+                    count1 ++;
+                }
+            }
+        }
+        System.out.println(count1);
 
 
     }
--- new/src/com/java/Les3.java
+++ new/src/com/java/Les3.java
@@ -2,5 +2,58 @@ package com.java;
 
 public class Les3 {
 
+//    public static boolean isLove(final int flower1, final int flower2) {
+//        if((flower1 % 2 == 0) && (flower2 % 2 == 1)){
+//            return true;
+//        };
+//        if((flower2 % 2 == 0) && (flower1 % 2 == 1)){
+//            return true;
+//        };
+//        return false;
+//
+//    }
+//    public static int findSmallestInt(int[] args) {
+//        int min = Integer.MAX_VALUE;
+//    for (int i = 0; i < args.length; i++){
+//        if (min> args[i]){
+//            min = args[i];
+//        }
+//    }
+//    return min;
+//    }public static String countingSheep(int num) {
+//    //Add your code here
+//    return "";
+//  }
 
+
+    public static int strCount(String str, char letter) {
+        int count = 0;
+       for (int i = 0; i<str.length(); i++){
+           if(str.charAt(i) == letter){
+               count++;
+           }
+       }
+       return count;
+    }
+
+    public static String countingSheep(int num) {
+        String result = "";
+        for (int i = 1; i<=num; i++ )
+            result += i + " sheep...";
+        return result;
+    }
+
+
+    public static String numberToString(int num) {
+        String a = Integer.toString(num);
+        return a;
+    }
+    public static void main (String[] args){
+
+
+        System.out.println(numberToString(999));
+
+        System.out.println(countingSheep(10));
+        System.out.println(strCount("hello world", 'l'));
+    }
 }
